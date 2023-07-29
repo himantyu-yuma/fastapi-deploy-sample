04eac3df9cb1c1e1803db5571aac50957e8f8f33
yuma-k <lipton.happy.milktea@gmail.com>
2023-07-29 11:16:02 +0900

    add: とりあえずAPIエンドポイント作成

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/api/ping")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/api/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
